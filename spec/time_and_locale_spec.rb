3720715c96e31284629bdc614c0b6f896b1e51ca
Yoshifumi Sato <sato_yos@nifty.com>
2021-05-04 11:06:16 +0900

    replace symbolic link to real file

--- spec/time_and_locale_spec.rb
+++ spec/time_and_locale_spec.rb
@@ -1,1 +1,10 @@
-../../ubuntu-popfile/spec/default/time_and_locale_spec.rb
+# coding: utf-8
+require 'spec_helper'
+
+describe command('date') do
+  its(:stdout) { should match /JST/}
+end
+
+describe command('echo $LANG') do
+  its(:stdout) { should match /ja_JP.UTF-8/}
+end
